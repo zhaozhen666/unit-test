afda0c17cc2f27ab304ae86c948bffe53ffb781f
zhaozhen <351848327@qq.com>
2023-03-07 13:09:18 +0800

    test @captor

--- unit-test-learn/src/test/java/com/example/ut/entity/UserServiceSpringTest.java
+++ unit-test-learn/src/test/java/com/example/ut/entity/UserServiceSpringTest.java
@@ -3,12 +3,10 @@ package com.example.ut.entity;
 import com.example.ut.mapper.UserDao;
 import com.example.ut.service.UserService;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
@@ -25,6 +23,14 @@ public class UserServiceSpringTest {
     @InjectMocks
     private UserService userService;
 
+    @Captor
+    private ArgumentCaptor<UserDO> argumentCaptor;
+
+    @Before
+    public void  beforeTest(){
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     public void  testCreateUser(){
         UserVO userVO = new UserVO();
@@ -39,4 +45,18 @@ public class UserServiceSpringTest {
         Assert.assertNotNull("用户实例不为空",userDO);
         Assert.assertEquals("用户id不相等",userDO.getId(),userVO.getId());
     }
+
+    @Test
+    public void  testCreateUserArgumentCaptor(){
+        UserVO userVO = new UserVO();
+        userVO.setId(1L);
+        userVO.setName("xiaoming");
+        userVO.setDesc("hao tongxue");
+        userService.modifyUser(userVO);
+        Mockito.verify(userDao).modify(argumentCaptor.capture());
+        UserDO userDO = argumentCaptor.getValue();
+        Assert.assertNotNull("用户实例不为空",userDO);
+        Assert.assertEquals("用户id不相等",userDO.getId(),userVO.getId());
+    }
+
 }
