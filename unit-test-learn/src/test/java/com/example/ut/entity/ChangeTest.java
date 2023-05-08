9bd019c64b79748734de9afd9ee65adf8fa887ef
zhaozhen <351848327@qq.com>
2023-05-09 06:36:23 +0800

    add algorithm

--- unit-test-learn/src/test/java/com/example/ut/entity/ChangeTest.java
+++ unit-test-learn/src/test/java/com/example/ut/entity/ChangeTest.java
@@ -6,4 +6,29 @@ package com.example.ut.entity;
  * @create: 2023/05/08 09:36
  */
 public class ChangeTest {
+
+    public static void main(String[] args) {
+      UserDO userDO =   new UserDO();
+      userDO.setName("zhao");
+      int i = 0;
+      userDO.setDesc("zhen1");
+      change(userDO.getName());
+      changeInt(i);
+      changeEntity(userDO);
+      System.out.println(userDO.getName());
+      System.out.println(i);
+      System.out.println(userDO.getDesc());
+    }
+
+    public static void change(String name) {
+        name = "zhen";
+    }
+
+    public static void changeEntity(UserDO userDO) {
+        userDO.setDesc("zhen2");
+    }
+
+    public static void changeInt(int i) {
+        i=2;
+    }
 }
